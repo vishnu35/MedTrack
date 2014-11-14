181d166d60aa5dc58f7235f89dadb892719061f4
nikhilarroa6 <nikhil1990arora@gmail.com>
2014-11-14 17:09:24 -0600

    z

--- gen/com/cs442team4/medtrack/R.java
+++ gen/com/cs442team4/medtrack/R.java
@@ -15,6 +15,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_launcher=0x7f030000;
+        public static final int asdf=0x7f030001;
+        public static final int xcvb=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;

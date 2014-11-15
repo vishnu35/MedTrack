d2aebf09732d62a320fb7963434b5d3b678d66f8
jskelic <jusufskelic@gmail.com>
2014-11-14 19:51:40 -0600

    test2

--- src/com/cs442team4/medtrack/LauncherActivity.java
+++ src/com/cs442team4/medtrack/LauncherActivity.java
@@ -2,8 +2,6 @@ package com.cs442team4.medtrack;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class LauncherActivity extends Activity {
 

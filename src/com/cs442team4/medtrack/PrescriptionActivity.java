230e299d0b445b870250b44a3ba57e8c868ef64f
vishnu35 <vishnuvardhank35@gmail.com>
2014-12-10 14:52:49 -0600

    gallery

--- src/com/cs442team4/medtrack/PrescriptionActivity.java
+++ src/com/cs442team4/medtrack/PrescriptionActivity.java
@@ -1,36 +1,130 @@
 package com.cs442team4.medtrack;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.cs442team4.medtrack.helper.SaveImage;
+
+import android.R.array;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 
+@SuppressWarnings("deprecation")
 public class PrescriptionActivity extends Activity {
+
+	final static int cameraData = 0;
+	Intent intent;
+	ArrayList<Uri> arraylist;
+	String DirectoryPath;
+	Gallery gallery;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_prescription);
 
+		gallery = (Gallery) findViewById(R.id.pgallery);
+		DirectoryPath = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/MedTracker/Images";
+
+		File[] pictures = GetFiles(DirectoryPath);
+		arraylist = getFileNames(pictures, DirectoryPath);
+		gallery.setAdapter(new ImageAdapter(this));
+	}
+
+	public File[] GetFiles(String directoryPath) {
+		File f = new File(directoryPath);
+		f.mkdirs();
+		File[] file = f.listFiles();
+		Arrays.sort(file);
+		return file;
+	}
+
+	public ArrayList<Uri> getFileNames(File[] file, String directoryPath) {
+		ArrayList<Uri> arrayFiles = new ArrayList<Uri>();
+		if (file.length == 0)
+			return null;
+		else {
+			Uri uri = null;
+			for (int i = file.length-1; i >= 0; i--) {
+				uri = Uri.parse("file://" + directoryPath + "/"
+						+ file[i].getName());
+				arrayFiles.add(uri);
+			}
+		}
+		return arrayFiles;
 	}
 
 	public void TakePic(View v) {
-		Intent intent = new Intent(
-				android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(intent, 0);
+		intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, cameraData);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		if (data != null) {
+		if (resultCode == RESULT_OK) {
+			if (data != null) {
+				Bundle extras = data.getExtras();
+				Bitmap bp = (Bitmap) extras.get("data");
+				SaveImage.Save(this, bp);
 
-			Bitmap bp = (Bitmap) data.getExtras().get("data");
-			ImageView TimageView1 = (ImageView) findViewById(R.id.TimageView1);
-			TimageView1.setVisibility(View.VISIBLE);
-			TimageView1.setImageBitmap(bp);
+				File[] pictures = GetFiles(DirectoryPath);
+				arraylist = getFileNames(pictures, DirectoryPath);
+				gallery.setAdapter(new ImageAdapter(this));
+			}
+		}
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+		private Context context;
+		int imgBg;
+
+		public ImageAdapter(Context context) {
+			this.context = context;
+
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return arraylist.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			ImageView imageview = new ImageView(context);
+			imageview.setLayoutParams(new Gallery.LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+			imageview.setScaleType(ScaleType.FIT_XY);
+			imageview.setImageURI(arraylist.get(position));
+			return imageview;
 		}
 	}
 }
---
+++ src/com/cs442team4/medtrack/helper/SaveImage.java
@@ -0,0 +1,71 @@
+package com.cs442team4.medtrack.helper;
+ 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+ 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+ 
+public class SaveImage {
+    private static Context TheThis;
+    private static String NameOfFolder = "/MedTracker/Images";
+    private static String NameOfFile   = "MT_";
+ 
+    public static void Save(Context context,Bitmap ImageToSave){
+        TheThis = context;
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath()+ NameOfFolder;
+        String CurrentDateAndTime= getCurrentDateAndTime();
+        File dir = new File(file_path);
+         
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+         
+        File file = new File(dir, NameOfFile +CurrentDateAndTime+ ".jpg");
+         
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+            ImageToSave.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            MakeSureFileWasCreatedThenMakeAvabile(file);
+        } 
+        catch (FileNotFoundException e) {}
+        catch (IOException e){}
+    }
+ 
+     
+ 
+    private static void MakeSureFileWasCreatedThenMakeAvabile(File file) {
+        MediaScannerConnection.scanFile(TheThis,
+                new String[] { file.toString() }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+            public void onScanCompleted(String path, Uri uri) {
+                Log.e("ExternalStorage", "Scanned " + path + ":");
+                Log.e("ExternalStorage", "-> uri=" + uri);
+                
+            }
+        });
+         
+    }
+ 
+ 
+ 
+    @SuppressLint("SimpleDateFormat")
+	private static String getCurrentDateAndTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String formattedDate = df.format(c.getTime());
+        return formattedDate;
+    }
+}
